c2b45776001bf4b96c833824c444ea66c963235c
Leonardo <1968682534@qq.com>
2019-01-21 22:49:35 +0800

    提交

--- src/JsonArray.js
+++ src/JsonArray.js
@@ -10,16 +10,16 @@ function JsonArray(a) {
 JsonArray.prototype.sort = JsonArray.prototype.sort ||
     /**
      * 对JSON数组按指定键的值进行排序
-     * @param {string} key 
+     * @param {string} keyName 
      * @param {boolean} desc 是否降序排列   默认false——采用升序排序
      * @return {number}
      */
-    function(key, desc) {
+    function(keyName, desc) {
         function _sort(a, b) {
-            if ('number' === typeof a[key]) {
-                return (!desc) ? a[key] - b[key] : b[key] - a[key];
-            } else if ('string' === typeof a[key]) {
-                var t = a[key].localeCompare(b[key]);
+            if ('number' === typeof a[keyName]) {
+                return (!desc) ? a[keyName] - b[keyName] : b[keyName] - a[keyName];
+            } else if ('string' === typeof a[keyName]) {
+                var t = a[keyName].localeCompare(b[keyName]);
                 return (!desc) ? t : (0 - t);
             } else
                 return -1;
@@ -27,9 +27,32 @@ JsonArray.prototype.sort = JsonArray.prototype.sort ||
         return this.arr.sort(_sort);
     }
 
-JsonArray.prototype.group = function() {
-
-}
+JsonArray.prototype.group = JsonArray.prototype.group ||
+    /**
+     * 以键名【keyName】字段对JSON数组进行分组（&排序）
+     * @param {string} keyName 
+     * @param {JSON} options 
+     */
+    function(keyName, options) {
+        var result = [],
+            keyValues = []; //键keyName对应的值的集合，不含重复值
+        for (var i = 0; i < this.arr.length; i++) {
+            var data = this.arr[i];
+            if (keyValues.indexOf(data[keyName]) < 0) keyValues.push(data[keyName]);
+            else continue;
+            var res = {
+                keyValue: data[keyName],
+                data: []
+            };
+            for (var j = i; j < this.arr.length; j++) {
+                data = this.arr[j];
+                if (keyValues[keyValues.length - 1] === data[keyName])
+                    res.data.push(data);
+            }
+            result.push(res);
+        }
+        return result;
+    }
 
 var JA = function(arr) {
     return new JsonArray(arr);
--- test/test.js
+++ test/test.js
@@ -1,13 +1,14 @@
 var JA = require('../src/JsonArray');
 
 var arr = [
-    { id: 15 },
-    { id: 4 },
-    { id: 6 },
-    { id: 7 }
+    { id: 15, name: 'sdd' },
+    { id: 6, name: 'sdd' },
+    { id: 4, name: 'ssddd' },
+    { id: 6, name: 'sdd' },
+    { id: 7, name: 'srtdd' }
 ];
 var JsonArray = JA(arr);
-var t = JsonArray.sort('id');
+var t = JsonArray.group('name');
 console.log(arr);
 console.log(JsonArray.srcArr);
 console.log(t);
